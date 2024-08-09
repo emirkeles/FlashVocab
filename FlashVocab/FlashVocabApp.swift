5fe868891de8c44278d1c8671b6750409f62f26e
emirkeles <16emirkeles@gmail.com>
2024-08-10 02:20:36 +0300

    update toast message

--- FlashVocab/FlashVocabApp.swift
+++ FlashVocab/FlashVocabApp.swift
@@ -8,23 +8,6 @@
 import SwiftUI
 import PopupView
 
-@Observable
-class ToastManager {
-    private init() {}
-    
-    var showToast = false
-    var toastIcon: String = "star.fill"
-    var toastTitle: String = "Yer işaretine eklendi"
-    var color: Color = .orange
-    
-    func showToast(icon: String, title: String) {
-            self.toastIcon = icon
-            self.toastTitle = title
-            self.showToast = true
-        }
-    
-    static let shared = ToastManager()
-}
 
 @main
 struct FlashVocabApp: App {
@@ -34,43 +17,16 @@ struct FlashVocabApp: App {
     var body: some Scene {
         WindowGroup {
             ContentView()
-                .modifier(ToastViewModifier(isPresented: $toastManager.showToast, icon: $toastManager.toastIcon, title: $toastManager.toastTitle, color: toastManager.color))
+                .modifier(
+                    ToastViewModifier(
+                        isPresented: $toastManager.showToast,
+                        icon: $toastManager.toastIcon,
+                        title: $toastManager.toastTitle,
+                        color: toastManager.color
+                    )
+                )
                 .flashCardDataContainer()
         }
-    }
-}
-
-struct ToastViewModifier: ViewModifier {
-    @Binding var isPresented: Bool
-    @Binding var icon: String
-    @Binding var title: String
-    var color: Color
-    
-    func body(content: Content) -> some View {
-        content
-            .popup(isPresented: $isPresented, view: {
-                ZStack {
-                    HStack {
-                        Image(systemName: icon)
-                            .resizable()
-                            .frame(width: 36, height: 36)
-                    }
-                    .frame(maxWidth: .infinity, alignment: .leading)
-                    
-                    Text(title)
-                        .font(.system(size: 18))
-                }
-                .foregroundColor(.white)
-                .padding(EdgeInsets(top: 56, leading: 16, bottom: 16, trailing: 16))
-                .frame(maxWidth: .infinity)
-                .background(color.gradient)
-            }, customize: {
-                $0
-                    .type(.toast)
-                    .position(.top)
-                    .animation(.spring)
-                    .autohideIn(0.8)
-            })
     }
 }
 
