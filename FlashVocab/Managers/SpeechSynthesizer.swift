57423f70f071b3d55a77847b003c006d218b9561
emirkeles <16emirkeles@gmail.com>
2024-08-29 10:46:33 +0300

    feat: add speech delegate to use callback when didFinish

--- FlashVocab/Managers/SpeechSynthesizer.swift
+++ FlashVocab/Managers/SpeechSynthesizer.swift
@@ -8,19 +8,29 @@
 import Foundation
 import AVFoundation
 
-class SpeechSynthesizer {
+class SpeechSynthesizer: NSObject, AVSpeechSynthesizerDelegate {
     private let synthesizer = AVSpeechSynthesizer()
     
     static let shared = SpeechSynthesizer()
     
-    private init() { }
+    private var completionCallBack: (() -> Void)?
     
-    func speak(_ text: String, language: String = "en-US", rate: Float = 0.1) {
+    private override init() {
+        super.init()
+        synthesizer.delegate = self
+    }
+    
+    func speak(_ text: String, language: String = "en-US", rate: Float = 0.1, callback: (() -> Void)? = nil) {
         let utterance = AVSpeechUtterance(string: text)
         if let voice = AVSpeechSynthesisVoice(language: language) {
             utterance.voice = voice
             utterance.rate = rate
+            completionCallBack = callback
             synthesizer.speak(utterance)
         }
+    }
+    func speechSynthesizer(_ synthesizer: AVSpeechSynthesizer, didFinish utterance: AVSpeechUtterance) {
+        completionCallBack?()
+        completionCallBack = nil
     }
 }
